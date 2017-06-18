46eb54edf5efac0421db1b36dda27310e3f17013
Kieron Dye <kdye@fraedom.com>
2017-06-18 12:15:23 +1200

    Changed console output to speech.

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Speech.Synthesis;
 
 namespace HelloWorld
 {
@@ -6,7 +7,8 @@ namespace HelloWorld
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+            var speaker = new SpeechSynthesizer();
+            speaker.Speak("Hello World!");
         }
     }
 }
